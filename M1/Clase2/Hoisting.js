2339cf99218058cd43c6bde2ddc38941218baeee
sangaray <scgaraycochea@yahoo.com.ar>
2024-08-28 19:46:24 -0300

    Tema 3 - Hoisting

---
+++ M1/Clase2/Hoisting.js
@@ -0,0 +1,28 @@
+// Tema 3 - Hoisting
+// Con Var
+console.log(x);
+var x = 5;
+console.log(x);
+
+// Con Const o Let
+// console.log(y); Error
+const y = 5;
+console.log(y);
+
+// console.log(z);
+let z = 5;
+console.log(z);
+
+if (true) {
+	console.log(v);
+}
+
+var v = 10;
+console.log(v);
+
+if (true) {
+	// console.log(w);
+}
+
+let w = 10;
+console.log(w);
