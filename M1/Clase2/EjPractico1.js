ff22a5184850e2198d037d4cca8c1e471d62e345
sangaray <scgaraycochea@yahoo.com.ar>
2024-08-28 19:28:55 -0300

    Ejemplos 1, 2, 3 de Temas 1 y 2

---
+++ M1/Clase2/EjPractico1.js
@@ -0,0 +1,30 @@
+// Ejercicios sobre Temas 1 y 2
+// Ejemplo 1
+var caja = 5;
+if (true) {
+	var caja = [];
+}
+console.log(caja);
+
+// Ejemplo 2
+var caja1 = 5;
+if (true) {
+	var caja1 = [];
+}
+if (true) {
+	let caja1 = "No hay cambios";
+}
+
+console.log(caja1);
+
+// Ejemplo 3
+// Ejemplo con Var
+for (var i = 0; i < 5; i++) {
+	setTimeout(function () {
+		console.log(i);
+	});
+}
+
+setTimeout(function () {
+	console.log("Valor final de 'i' usando 'var': " + i);
+}, 6000);
