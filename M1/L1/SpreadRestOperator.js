1680e69726c72d27c4969b4e2dfc9205f93fa753
sangaray <scgaraycochea@yahoo.com.ar>
2024-09-02 19:44:24 -0300

    Repasando M1

--- M1/L1/SpreadRestOperator.js
+++ M1/L1/SpreadRestOperator.js
@@ -1,5 +1,39 @@
-// Tema 10
-// Spread Operator
+// Spread Operator (Video 7)
+// Nos facilita ciertastareas que llevarían más código sin este operador
+// Si quiero crear una copia de este objeto, cuando igualo dos objetos, lo que hago es referenciar el nuevo objeto al mismo lugar donde se encuentra el primero, porque la copia es por referencia, en realidad es el mismo, no se hace una copia independiente o distinta. Para crear una copia tengo que usar el spread operator.
+// Este aplica tanto a objetos como a arrays
+// Se puede usar también para cambiar propiedades
+
+const perro = {
+	raza: "Salchicha",
+	nombre1: "Pancho",
+	edad1: 3,
+	color: "marrón",
+};
+
+perro.collar = true;
+console.log(perro);
+
+perro2 = { ...perro, collar: false };
+console.log(perro2);
+console.log(perro);
+
+const miArreglo = [true, "palabra", { a: 5 }];
+
+miArreglo.push("Nuevo elemento");
+console.log(miArreglo);
+const miArreglo2 = [...miArreglo, "otro elemento"];
+console.log(miArreglo2);
+console.log(miArreglo);
+
+// Para concatenar arreglos
+const miArreglo3 = [4, 8, 89, 1];
+const miArreglo4 = [45, 78, 12, 3];
+const miArreglo5 = [...miArreglo3, ...miArreglo4];
+console.log(miArreglo5);
+
+// Ejemplo Teoría de Henry
+
 const persona = {
 	nombre: "Severus",
 	apellido: "Snape",
