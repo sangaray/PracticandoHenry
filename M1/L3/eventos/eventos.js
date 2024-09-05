39b2916cbfb6f37a24ed68822fceec082974036b
sangaray <scgaraycochea@yahoo.com.ar>
2024-09-05 15:25:17 -0300

    M1-L3 - DOM y Eventos

--- M1/L3/eventos/eventos.js
+++ M1/L3/eventos/eventos.js
@@ -1,16 +1,18 @@
 /* Eventos 
-  id = cuando quiero identificar un objeto en el DOM especificamente
-
+  id = cuando quiero identificar un objeto en el DOM especificamente, en js
   Cuando pase esto quiero que hagas esto otro
 
-  getElementByTagName es un array
+  getElementByTagName es un array de elementos html
+
+  addEventListener("evento", función callback)
+  La función si es pequeña se pone en el argumento y si es larga la pongo aparte
 */
 
 const agregar = document.getElementById("agregar");
-console.log(agregar);
 const body = document.getElementsByTagName("body")[0];
 
 /*const cb = () => console.log("Hicimos click en el botón o probando...")
+
 
 La cb recibe sin que hagams nada un evento, el mismo evento que acaba de dispararse que tiene propiedades, una de ellas es target, esta es el elemento que disparó el evento
 
@@ -18,12 +20,14 @@ La cb recibe sin que hagams nada un evento, el mismo evento que acaba de dispara
 
 const cb = () => {
 	const nuevoDiv = document.createElement("div");
-	console.log(nuevoDiv);
 	nuevoDiv.classList.add("cuadrado");
 	nuevoDiv.classList.add("rojo");
-  nuevoDiv.addEventListener("click", (event) => event.target(remove());
 
-  body.appendChild("nuevoDiv")
+	nuevoDiv.addEventListener("click", (event) => {
+		event.target.remove();
+	});
+
+	body.appendChild(nuevoDiv);
 };
 
 agregar.addEventListener("click", cb);
