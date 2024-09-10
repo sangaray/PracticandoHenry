ed2d650f657cee82d4390d53ea21b3e203b9c496
sangaray <scgaraycochea@yahoo.com.ar>
2024-09-10 15:12:16 -0300

    M1-L4 - Testing: Primer prueba de testing

---
+++ M1/L4/ClasePractica/spec/prueba.spec.js
@@ -0,0 +1,20 @@
+// M1-L4 - Testing
+// Permite realizar pruebas sin necesidad de cargar datos de prueba manualmente
+// Probar determinadas funcionalidades de nuestra aplicación
+// Jasmine, librería de testing. Nos brinda una serie de métodos para realizar las pruebas unitarias
+// Ventajas de usar testing
+// Detección temprana de errores, permite darme cuenta si algo está mál en el código antes que se vuelva difícil encontar ese error
+// Facilita el mantenimiento de código, nos permite encontrar fácilmente donde está el error
+// Facilidad para implementar cambios, si queremos hacer un cambio que aunqueno cambie el resultado va a cambiar el proceso, me ayuda a asegurarm que no cambia el resultado
+// Integración y despliegue continuos (CI/CD), permite a medida que desarrollamos que nuestras funcionalidades pasen las pruebas que verificand que no haya problemas
+// Facilita el refactorizado del código, el cambio puede hace cambios en el proceso, también se puede hacer con más facilidad y tranquilidad de que no se afecte el resultado de las funcionalidades
+// JASMINE
+// Framework de pruebas unitarias
+// Provee un entorno de trabajo, es un pequeño taller con un montón de herramientas para realizar un trabajo
+// describe() grupo de pruebas que voy a realizar, que tienen el mismo objetivo o funcionalidad
+
+describe("Este es mi primer grupo de tests", () => {
+	it("El número 2 debe ser igual al número 2", () => {
+		expect(2).toEqual(2);
+	});
+});
