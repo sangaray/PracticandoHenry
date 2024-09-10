8b782c35f943731903f53ad1ffb2defc6b3ad29c
sangaray <scgaraycochea@yahoo.com.ar>
2024-09-10 18:44:03 -0300

    M1-L4 - Testing: Primer prueba de testing Completada

--- M1/L4/ClasePractica/spec/prueba.spec.js
+++ M1/L4/ClasePractica/spec/prueba.spec.js
@@ -14,6 +14,11 @@
 // describe(string, callback) grupo de pruebas que voy a realizar, que tienen el mismo objetivo o funcionalidad. String, descripción. Callback, conjunto de test individuales específicos que se van a aplicar a ese grupo
 // it(string, callback), dice que voy aevaluar especificamente, indica un test específico. String, descripción del test específico, prueba que va a realizar
 // expect(expresión a evaluar).prueba a aplicar(valor de comparación),
+// Al hacer las pruebas evaluar sobre casos conocidos, resultados que sabemos que son correctos
+// toBe(), indica que un valor dado es o da por resultado otro valor
+// toEqual(), realiza una comparación profunda para buscar igualdades
+// Cuando trabajamos con objetos o arrays se usa toWqual() en lugar de toBe()
+// toBeDefined(), determina si una función o variables está definida
 
 describe("Este es mi primer grupo de tests", () => {
 	it("El número 2 debe ser igual al número 2", () => {
@@ -21,5 +26,64 @@ describe("Este es mi primer grupo de tests", () => {
 	});
 	it("El número 2 debe estar definido", () => {
 		expect(2).toBeDefined();
+	});
+});
+
+const sumar = (a, b) => a + b;
+
+describe("La función sumar", () => {
+	it("Debe estar definida", () => {
+		expect(sumar).toBeDefined();
+	});
+
+	it("Debe sumar dos números enviados como argumentos", () => {
+		expect(sumar(2, 2)).toBe(4);
+		expect(sumar(4, 4)).toBe(8);
+		expect(sumar(5, 0)).toBe(5);
+	});
+});
+
+describe("Una prueba con objetos", () => {
+	it("Espero que dos números iguales sean iguales", () => {
+		expect(5).toBe(5);
+		expect(10).toBe(10);
+	});
+
+	it("Espero que dos palabras iguales sean iguals", () => {
+		expect("Jorge").toBe("Jorge");
+		expect("Alejo").toBe("Alejo");
+	});
+
+	it("Espero que dos objetos iguales sean iguales", () => {
+		expect({ nombre: "Jorge" }).toEqual({ nombre: "Jorge" });
+	});
+
+	it("Espero que dos arrays sean iguales", () => {
+		expect(["Jorge"]).toEqual(["Jorge"]);
+	});
+});
+
+// Recibe un array de números y devuelve un array de números pares
+// Puede ser que no tenga la función pero que yo sepa las especificaciones
+// Debe estar definida
+// Debe retornar un array
+// Debe retornar un array con los números pares
+// y a partir de esas especificaciones, creo primero los tests y luego creo la función para que responda a esos tests
+// Este modelo funciona así, teniendo primero los test y luego desarrollar la función, pasando los test una y otra vez hasta que todos se cumplan, esto nos va a asegurar que la función fue bien implementada y que es funcional
+const filtrar = (arr) => arr.filter((num) => num % 2 === 0);
+
+describe("La función filtrar", () => {
+	it("Debe estar definida", () => {
+		expect(filtrar).toBeDefined();
+	});
+
+	it("Debe retornar un array", () => {
+		expect(Array.isArray(filtrar([1, 2, 3, 4, 5, 6]))).toBe(true);
+	});
+	it("Debe retornar un array con los números pares", () => {
+		expect(filtrar([1, 2, 3, 4])).toEqual([2, 4]);
+		expect(filtrar([2, 4, 6])).toEqual([2, 4, 6]);
+		expect(filtrar([1, 3, 5, 7, 9])).toEqual([]);
+		expect(filtrar([])).toEqual([]);
 	});
 });
