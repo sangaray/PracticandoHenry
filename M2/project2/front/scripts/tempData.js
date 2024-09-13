0b034365f651ec0c754033071ecb4e1743f8304a
sangaray <scgaraycochea@yahoo.com.ar>
2024-09-13 16:52:46 -0300

    Project 2 - Barra de navegación completa

---
+++ M2/project2/front/scripts/tempData.js
@@ -0,0 +1,29 @@
+const template = [
+	{
+		title: "Guardians of the Galaxy Vol. 2",
+		year: 2017,
+		director: "James Gunn",
+		duration: "2h 16min",
+		genre: ["Action", "Adventure", "Comedy"],
+		rate: 7.7,
+		poster: "",
+	},
+	{
+		title: "Star Wars: Episode IV - A new Hope",
+		year: 1977,
+		director: "George Lucas",
+		duration: "2h 1min",
+		genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
+		rate: 8.7,
+		poster: "",
+	},
+	{
+		title: "The Lord of the Rings: The Fellowship of the King",
+		year: 2001,
+		director: "Peter Jackson",
+		duration: "2h 58min",
+		genre: ["Action", "Adventure", "Drama", "Fantasy"],
+		rate: 8.8,
+		poster: "",
+	},
+];
