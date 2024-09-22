b6fe59c05f543b19bdcc5fc2690c39b453cc1646
sangaray <scgaraycochea@yahoo.com.ar>
2024-09-22 19:25:07 -0300

    M2-L5 - Demo-Jest iniciado

--- M2/L5/demo-jest/index.js
+++ M2/L5/demo-jest/index.js
@@ -1,2 +1,6 @@
 // M2-L5 - Testing Advanced
 // Jest, lo instalamos como dependencia de desarrollo
+// M2-L5 - Testing Avanzado
+// Para instalar una dependencia de desarrollo ponemos -D delante del nombre de la dependencia
+// Ya tiene especificada la configuración para que busque los archivos de tests, esto lo trae por defecto
+// Siempre tenemos que tener una carpeta tests y los archivos deben nombrarse como *.test.js
