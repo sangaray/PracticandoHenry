1061b743215539ab7974a3173fdeb85cb7d30895
sangaray <scgaraycochea@yahoo.com.ar>
2024-09-23 00:34:54 -0300

    M2-L5 - Demo-Jest con mock functions simples  completado

--- M2/L5/demo-jest/index.js
+++ M2/L5/demo-jest/index.js
@@ -5,6 +5,11 @@
 // Ya tiene especificada la configuración para que busque los archivos de tests, esto lo trae por defecto. No necesito indicarle donde se encuentran los módulos de tests, sólo la extensión test o spec y él los encuentra solo en la carpeta tests
 // Siempre tenemos que tener una carpeta tests y los archivos deben nombrarse como *.test.js o *.spec.js
 // jest no requiere prácticamente de configuración, es mucho más sencillo que jasmine, su output es más amigable.
+// Mock Functions, es una copia de una función que puedo testear fuera de la aplicación completa, sólo para saber si funciona o no.
+// Para hacer esto escribo lo siguiente:
+// const mockSumar = jest.fn(sumar);
+// Para que jest muestre los console log debo correr primero en la consola
+// npm test -- --silent=false
 
 const sumar = (a, b) => {
   if (typeof a !== "number" || typeof b !== "number") return null;
--- M2/L5/demo-jest/tests/sumar.test.js
+++ M2/L5/demo-jest/tests/sumar.test.js
@@ -1,5 +1,7 @@
 const sumar = require("../index");
 
+const mockSumar = jest.fn(sumar);
+
 /* describe("Una prueba de tests que pasan siempre", () => {
   test("Este test debe pasar siempre", () => {
     expect(true).toBe(true);
@@ -8,15 +10,21 @@ const sumar = require("../index");
 
 describe("La función 'sumar'", () => {
   it("Debe retornar null si es ejecutada con algún valor no numérico como argumento", () => {
-    expect(sumar(1, true)).toBe(null);
-    expect(sumar(false, 10)).toBe(null);
-    expect(sumar("hola", "chau")).toBe(null);
+    expect(mockSumar(1, true)).toBe(null);
+    expect(mockSumar(false, 10)).toBe(null);
+    expect(mockSumar("hola", "chau")).toBe(null);
+    // console.log(mockSumar.mock);
   });
 
   it("Debe retornar correctamente el resultado de la suma", () => {
-    expect(sumar(10, 15)).toBe(25);
-    expect(sumar(0, 0)).toBe(0);
-    expect(sumar(-8, 8)).toBe(0);
-    expect(sumar(-20, -30)).toBe(-50);
+    expect(mockSumar(10, 15)).toBe(25);
+    expect(mockSumar(0, 0)).toBe(0);
+    expect(mockSumar(-8, 8)).toBe(0);
+    expect(mockSumar(-20, -30)).toBe(-50);
+    // console.log(mockSumar.mock);
+  });
+
+  it("Debe haber registrao correctamente los argumentos 10 y 15", () => {
+    expect(mockSumar).toHaveBeenCalledWith(10, 15);
   });
 });
