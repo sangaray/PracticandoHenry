d28c0afb5cd33025aba5918ccce823d691584b76
sangaray <scgaraycochea@yahoo.com.ar>
2024-09-25 14:20:57 -0300

    M2-L6 - Trabajando con Promesas terminado

---
+++ M2/L6/DemoPromesa/index.js
@@ -0,0 +1,36 @@
+// Promesas
+// Es el eventual resultado de una operación asíncrona, normalmente no sabemos lo que puede pasar, puede ser exitoso o puede devolver un error, no hay un único resultado posible.
+// Es una clase de la cual tenemos que crear una instancias. Tie ne una callback que reciben dos funciones resolve y reject, la primera resulve la promesa, es decir es exitosa, y la segunda es el rechazo de la primesa o si hay un error por alguna razón que no dejó completar el proceso que se estaba ejecutando.
+// El resolve se maneja con .then y el reject con .catch
+// err.message, resume el mensaje de error a la causa específica del mismo
+
+// Probando como funcionan las promesas
+
+const axios = require("axios");
+
+/* const promesa = new Promise((resolve, reject) => {});
+
+console.log(promesa);
+
+const promesaResuelta = new Promise((resolve, reject) => {
+  resolve("Valor de resolución");
+});
+
+const promesaRechazada = new Promise((resolve, reject) => {
+  reject("Razón del rechazo");
+});
+ */
+// Promesa real
+const promise = axios.get("https://students-api.up.railway.app/movies");
+
+promise
+  .then((res) => {
+    // success handler
+    console.log(res.data);
+  })
+  .catch((err) => {
+    // error handler
+    console.log(err.message);
+  });
+
+// console.log(promise);
