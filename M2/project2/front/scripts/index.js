6536a4d7875dd2dd6b31e617f1929000f483259f
sangaray <scgaraycochea@yahoo.com.ar>
2024-09-25 18:39:28 -0300

    M2-L6 - Project 2 migración de jquery a axios terminada

--- M2/project2/front/scripts/index.js
+++ M2/project2/front/scripts/index.js
@@ -2,11 +2,23 @@
 // import { template } from "./tempData.js";
 
 // Obtener el contenedor donde se agregarán las cards
+// vamos a cambiar la petición jquery por una petición con axios
+// $.get(apiUrl, (data) => {
+//console.log(data);
+//renderAllMovies(data);
+//});
+const axios = require("axios");
 const renderAllMovies = require("./renderAllMovies");
 
 const apiUrl = "https://students-api.up.railway.app/movies";
 
-$.get(apiUrl, (data) => {
-  console.log(data);
-  renderAllMovies(data);
-});
+const fetchData = async () => {
+  try {
+    const data = await axios.get(apiUrl);
+    renderAllMovies(data.data);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+fetchData();
