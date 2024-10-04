bfb343efbeb779ea24dd37cfdba166cde077e9ca
sangaray <scgaraycochea@yahoo.com.ar>
2024-10-04 20:08:07 -0300

    M2-L11 - Demo Mongoose conexión a la base de datos establecida exitosamente

--- M2/L11/index.js
+++ M2/L11/index.js
@@ -1,7 +1,11 @@
 // App que pone en escucha a nuestro servidor en el puerto 3000
 
 const app = require("./src/server");
+const dbCon = require("./src/config/dbCon");
+require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log("Servidor escuchando en el puerto 3000");
+dbCon().then(() => {
+  app.listen(3000, () => {
+    console.log("Servidor escuchando en el puerto 3000");
+  });
 });
