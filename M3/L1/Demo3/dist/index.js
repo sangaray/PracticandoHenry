fb1a4a22fc2c40366fb4004596254784969a2f89
sangaray <scgaraycochea@yahoo.com.ar>
2024-12-20 15:02:40 -0300

    M3-L1 - Demo3 - Trabajando con Analisis de código estático

--- M3/L1/Demo3/dist/index.js
+++ M3/L1/Demo3/dist/index.js
@@ -1,10 +1,8 @@
+"use strict";
 // En la tsconfig.json hay una opción noImplicitAny, que dice no te voy a dejar tener ninguna variable dando vueltas que no se sepa claramente que tipo de datos es.
 // num1 y num2 al estar inicializadas se deduce el tipo de dato que tienen, pero para a y b no hay ninguna posible inferencia ni aclaración del tipo de dato que son.
 // Supongamos que mi aplicación es muy grando y estoy infiriendo un tipo de dato erróneo y num1 en lugar de ser 5, un número es un string. TS va a indicarme que hay un error.
-
 const num1 = 20;
 const num2 = 10;
-
-const sumar = (a: number, b: number) => a + b;
-
+const sumar = (a, b) => a + b;
 console.log(sumar(num1, num2));
