acec705df76269ded918465e22bc9b0414d8e314
sangaray <scgaraycochea@yahoo.com.ar>
2024-12-22 18:25:12 -0300

    Project3 - Configurando TS y practicando con el código

---
+++ M3/Project3/back/dist/index.js
@@ -0,0 +1,39 @@
+"use strict";
+let num1 = 15;
+const num2 = 32;
+num1 = 28;
+console.log(num1 + num2);
+const pet1 = {
+    name: "Luna",
+    age: 3,
+    color: "Gray",
+    kind: "Cat",
+};
+const pet2 = {
+    name: "Max",
+    age: 5,
+    color: "Brown",
+    kind: "Dog",
+};
+const pet3 = {
+    name: "Kiwi",
+    age: 2,
+    color: "Green",
+    kind: "Parrot",
+};
+const pet4 = {
+    name: "Goldie",
+    age: 1,
+    color: "Gold",
+    kind: "Fish",
+};
+const pet5 = {
+    name: "Snow",
+    age: 4,
+    color: "White",
+    kind: "Rabbit",
+};
+// Agrupando todos los objetos en un array
+const petsArray = [pet1, pet2, pet3, pet4, pet5];
+// Imprimiendo el array completo
+console.log(petsArray);
---
+++ M3/Project3/back/index.ts
@@ -0,0 +1,54 @@
+let num1 = 15;
+const num2 = 32;
+
+num1 = 28;
+
+console.log(num1 + num2);
+
+interface IPets {
+  name: string;
+  age: number;
+  color: string;
+  kind: string;
+}
+
+const pet1: IPets = {
+  name: "Luna",
+  age: 3,
+  color: "Gray",
+  kind: "Cat",
+};
+
+const pet2: IPets = {
+  name: "Max",
+  age: 5,
+  color: "Brown",
+  kind: "Dog",
+};
+
+const pet3: IPets = {
+  name: "Kiwi",
+  age: 2,
+  color: "Green",
+  kind: "Parrot",
+};
+
+const pet4: IPets = {
+  name: "Goldie",
+  age: 1,
+  color: "Gold",
+  kind: "Fish",
+};
+
+const pet5: IPets = {
+  name: "Snow",
+  age: 4,
+  color: "White",
+  kind: "Rabbit",
+};
+
+// Agrupando todos los objetos en un array
+const petsArray: IPets[] = [pet1, pet2, pet3, pet4, pet5];
+
+// Imprimiendo el array completo
+console.log(petsArray);
