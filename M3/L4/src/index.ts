0afd8693e17cc245cf006743d3275afed86886d9
sangaray <scgaraycochea@yahoo.com.ar>
2024-12-23 16:36:24 -0300

    M3-L4 - Demo Typescript - Creamos y configuramos los typescript para un nuevo proyecto backend

---
+++ M3/L4/src/index.ts
@@ -0,0 +1,17 @@
+// Pasos de Preparación del proyecto
+// 1. inicializar el proyecto (npm init -y)
+// 2. Crear carpeta del proyecto
+// 3. Crear carpeta src
+// 4. instalar las dependencias (npm install --save-dev typescript @types/express @types/node nodemon ts-node express)
+// 5.Inicializar tsconfig.json (npx tsc --init)
+// 6. Configurar tsconfig.json
+// Las siguientes configuraciones deben estar siempre en tsconfig.json
+// "target": "es6"
+// "module": "commonjs"
+// "outDir": "./dist"
+// "rootDir": "./src"
+// "strict": true
+// "include": ["src/**/*.ts"], // archivos a compilar
+// "exclude": ["node_modules"] // que no se va a compilar
+// 7. Crear el archivo index.ts
+// 8. Crear el archivo .gitignore
