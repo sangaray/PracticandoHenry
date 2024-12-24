3a65a9636c7041903f06003688ae59b8c475c619
sangaray <scgaraycochea@yahoo.com.ar>
2024-12-24 18:34:02 -0300

    M3-L4 - Se crearon los controllers

--- M3/L4/src/controllers/usersController.ts
+++ M3/L4/src/controllers/usersController.ts
@@ -1,1 +1,7 @@
 // Vamos a encontra que en algunos casos se usa Users.Controllers.ts para nombrar a los archivos controladores
+
+export const getUsers = async () => {};
+
+export const createUser = () => {};
+
+export const deleteUser = () => {};
