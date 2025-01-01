22067ac8ccad8e93ccf05daa198961c8090c8b3c
sangaray <scgaraycochea@yahoo.com.ar>
2025-01-01 20:26:33 -0300

    Project3 - Servicios para appointments completaddo

--- M3/Project3/back/src/dtos/IAppointmentDto.ts
+++ M3/Project3/back/src/dtos/IAppointmentDto.ts
@@ -1,12 +1,9 @@
 import AppointmentStatus from "../enums/appointmentStatus";
-
-interface IAppointment {
-  id: number;
+interface IAppointmentDto {
   date: Date; // 2024-12-28
   time: string; // 14:30
-  userId: number;
   status: AppointmentStatus;
   description: string;
 }
 
-export default IAppointment;
+export default IAppointmentDto;
