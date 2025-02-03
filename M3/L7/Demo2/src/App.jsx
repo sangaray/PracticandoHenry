799b0a3e9548e264d2304ec3110a958f11d052e0
sangaray <scgaraycochea@yahoo.com.ar>
2025-02-03 16:51:41 -0300

    M3-L7 - Demo2: Trabajamos Renderizado Condicional

--- M3/L7/Demo2/src/App.jsx
+++ M3/L7/Demo2/src/App.jsx
@@ -1,19 +1,27 @@
 {
+  /* Renderizado Condicional
   /* Para agregar código js debo ponerlo entre llaves */
-  /* El valor no cambia haciendolo de esta manera, para que cambie el contado necesitamos los estados */
+  /* El valor no cambia haciendolo de esta manera, para que cambie el contado necesitamos los estados 
+  /* Para reducir código vamos a utilizar el operador && (and)
+  /* Para establecer la condición puedo usar:
+  /* Si lógico: if(isRegistered) {return <Login/>}
+  /* Operador And (&&): {isRegistered && <Login/>} {!isRegistered && <Register/>}
+  /* Operador Ternario: {isRegistered ? <Login/> : <Register/>}
+
+  */
 }
 
 import "./App.css";
-import CardList from "./components/CardList";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
-  let counter = 0;
+  const isRegistered = true;
+
   return (
     <>
-      <h1>Mi Primera App</h1>
-      <CardList />
-      <p>Clicks: {counter}</p>
-      <button onClick={() => counter++}>Contador de Clicks</button>
+      <h1>Bienvenido a mi Primera App</h1>
+      {isRegistered ? <Login /> : <Register />}
     </>
   );
 }
