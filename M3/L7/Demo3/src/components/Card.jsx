99d8f8c2edcaab82b96c847cbeb2e7fb2746f0ba
sangaray <scgaraycochea@yahoo.com.ar>
2025-02-03 19:02:29 -0300

    M3-L7 - Demo3: Trabajamos tres formas de dar estilos, css legacy, modules y styled components

--- M3/L7/Demo3/src/components/Card.jsx
+++ M3/L7/Demo3/src/components/Card.jsx
@@ -1,8 +1,9 @@
+/* Cuando trabajamos con componentes en lugar de usar class, usamos classname como atributo de la etiqueta */
 const Card = ({ title, description }) => {
   return (
     <div>
       {/* se usa <div></div> o un <></> fragment */}
-      <h1>Card</h1>
+      <h1 className="title">Card</h1>
       <h3>Nombre: Dai</h3>
       <p>Email: daiana@gmail.com</p>
     </div>
