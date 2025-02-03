9e35e4aba70c5819b08cde89389c245c81650216
sangaray <scgaraycochea@yahoo.com.ar>
2025-02-03 15:46:49 -0300

    M3-L7 - Demo1: Se crearon dos componentes uno de prueba y otro para trabajar

--- M3/L7/Demo1-ViteProject/vite-react/src/App.jsx
+++ M3/L7/Demo1-ViteProject/vite-react/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+{
+  /* Para agregar código js debo ponerlo entre llaves */
+  /* El valor no cambia haciendolo de esta manera, para que cambie el contado necesitamos los estados
 }
 
-export default App
+import "./App.css";
+import CardList from "./components/CardList";
+
+function App() {
+  let counter = 0;
+  return (
+    <>
+      <h1>Mi Primera App</h1>
+      <CardList />
+      <p>Clicks: {counter}</p>
+      <button onClick={() => counter++}>Contador de Clicks</button>
+    </>
+  );
+}
+
+export default App;
+
