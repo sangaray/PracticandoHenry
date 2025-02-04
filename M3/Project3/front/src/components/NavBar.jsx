579f08d6cc6e8f10bb94a905e6a40a3a33b3163d
sangaray <scgaraycochea@yahoo.com.ar>
2025-02-04 17:59:50 -0300

    Project3 - Frontend: Creamos un componente NavBar

---
+++ M3/Project3/front/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <>
+      <h2>Este es el componente NavBar</h2>
+    </>
+  );
+};
+
+export default NavBar;
--- M3/Project3/front/src/views/Home.jsx
+++ M3/Project3/front/src/views/Home.jsx
@@ -1,7 +1,10 @@
+import NavBar from "../components/NavBar";
+
 const Home = () => {
   return (
     <div>
       <h1>Este es el componente Home</h1>
+      <NavBar />
     </div>
   );
 };
