7e42b3003d1c3558c68a9ce2fc4678eeb810e1a2
sangaray <scgaraycochea@yahoo.com.ar>
2025-02-06 00:49:38 -0300

    Project3 - Se crearon los componentes para la navbar y se dio estilos al home

--- M3/Project3/front/src/components/NavBar.jsx
+++ M3/Project3/front/src/components/NavBar.jsx
@@ -3,11 +3,11 @@ import NavLinks from "./NavLinks";
 
 const NavBar = () => {
   return (
-    <>
+    <div>
       <h2>Este es el componente NavBar</h2>
       <LogoEmpresa />
       <NavLinks />
-    </>
+    </div>
   );
 };
 
