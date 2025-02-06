1475ab46ecd9e28ce0a263285b668b1f142d3f50
sangaray <scgaraycochea@yahoo.com.ar>
2025-02-06 15:50:02 -0300

    M3-L8 - Daina-Demo1: Creamos nuestro primer Hook

---
+++ M3/L8/Daiana Grilla/Demo1/src/components/CustomButton.js
@@ -0,0 +1,13 @@
+import styles from "styled-components";
+
+export const CustomButton = styles.button`color: black;
+font-size: 1.5rem;
+margin: 1rem;
+padding: 0.25rem 1rem; 
+border-radius: 0.5rem;
+border: none;
+
+&:hover {
+    background-color: goldenrod;
+
+}`;
