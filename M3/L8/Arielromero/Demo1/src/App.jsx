4bc59bf0189c6218047bd4c5c560e42d68158d1c
sangaray <scgaraycochea@yahoo.com.ar>
2025-02-10 16:28:21 -0300

    M3-L8 - Ariel - Demo ToDo List: creamos un nuevo proyecto y el primer comopnente

---
+++ M3/L8/Arielromero/Demo1/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ToDoCounter from "./components/ToDoCounter";
+
+function App() {
+  return (
+    <>
+      <h1>ToDo List</h1>
+      <hr />
+      <ToDoCounter />
+    </>
+  );
+}
+
+export default App;
+
---
+++ M3/L8/Arielromero/Demo1/src/components/ToDoCounter.jsx
@@ -0,0 +1,16 @@
+import "./../index.css";
+
+const ToDoCounter = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#5f48f5",
+        borderRadius: "10px",
+        padding: "5px",
+      }}>
+      <h2>ToDo Counter: Componente Hijo</h2>
+    </div>
+  );
+};
+
+export default ToDoCounter;
