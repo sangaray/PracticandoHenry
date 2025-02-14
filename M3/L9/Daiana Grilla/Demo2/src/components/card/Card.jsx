a9b7b66c513aee8ebec56d842248801449c28b80
sangaray <scgaraycochea@yahoo.com.ar>
2025-02-14 14:45:59 -0300

    M3-L9 - Daiana-Demo2: Se creo un componente details modal y sus estilos

--- M3/L9/Daiana Grilla/Demo2/src/components/card/Card.jsx
+++ M3/L9/Daiana Grilla/Demo2/src/components/card/Card.jsx
@@ -1,11 +1,11 @@
 const Card = ({ character, handlerOnClick }) => {
-  const { name, house, id, hogwartsStaff } = character;
+  const { name, house, id, hogwartsStaff, hogwartsStudent } = character;
 
   return (
     <div>
       <h2>{name}</h2>
       <h3>{house}</h3>
-      <h3>{hogwartsStaff ? "Staff" : "Other"}</h3>
+      <h3>{hogwartsStaff ? "Staff" : hogwartsStudent ? "Student" : "Other"}</h3>
       <button onClick={() => handlerOnClick(id)}>Ver Detalle</button>
     </div>
   );
