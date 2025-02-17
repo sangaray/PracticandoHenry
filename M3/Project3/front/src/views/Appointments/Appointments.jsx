65e7ae3f7e8167c45266f5674fb01540682dffbc
sangaray <scgaraycochea@yahoo.com.ar>
2025-02-17 18:32:07 -0300

    Project3 - Se agregaron los estados necesarios para conectar el frontend con el backend, y se realizó la conexión

--- M3/Project3/front/src/views/Appointments/Appointments.jsx
+++ M3/Project3/front/src/views/Appointments/Appointments.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
-import appointments from "../../helpers/appointmentsDB";
+// import appointments from "../../helpers/appointmentsDB";
 import AppointmentCard from "../../components/appointmentCard/AppointmentCard";
 import styles from "./Appointments.module.css";
+import { useEffect } from "react";
+import axios from "axios";
+
+const GET_APPOINTMENTS_URL = "http://localhost:3000/appointments";
 
 const Appointments = () => {
-  const [appointmentsList, setAppointmentsList] = useState(appointments);
+  const [appointmentsList, setAppointmentsList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(GET_APPOINTMENTS_URL)
+      .then((response) => response.data)
+      .then((dbData) => setAppointmentsList(dbData))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <>
       <h2>Tus Turnos</h2>
       <div className={styles.container}>
-        {appointments.map((appointment) => (
+        {appointmentsList.map((appointment) => (
           <div className={styles.itemsContainer} key={appointment.id}>
             <AppointmentCard
               id={appointment.id}
