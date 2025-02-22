a60e8efba13650c8f8bce8e0a871157c0378a280
sangaray <scgaraycochea@yahoo.com.ar>
2025-02-22 15:29:54 -0300

    M3-L10 - Ariel-Demo1: Trabajamos con formularios controlados, validación de datos y regex

--- M3/L10/ArielRomero/Demo1/src/componets/formLogin/FormLogin.jsx
+++ M3/L10/ArielRomero/Demo1/src/componets/formLogin/FormLogin.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { validateUser } from "../../helpers/validateUser";
 
 export default function FormLogin() {
   const [input, setInput] = useState({
     username: "",
     password: "",
   });
+
+  const [errors, setErrors] = useState({
+    username: "Username es requerido",
+    password: "Password es requerido",
+  });
+
   const handleChange = (event) => {
     // console.log(event.target.value);
     // console.log(event.target.name); así sé desde qué input estoy se invocó la función
@@ -14,6 +21,13 @@ export default function FormLogin() {
       // [name]: value,
       [event.target.name]: event.target.value,
     });
+    setErrors(
+      validateUser({
+        ...input,
+        // [name]: value,
+        [event.target.name]: event.target.value,
+      })
+    );
   };
 
   const handleSubmit = (event) => {
@@ -39,6 +53,12 @@ export default function FormLogin() {
           placeholder="Ingresa tu email"
           onChange={handleChange}
         />
+        <br />
+        <span style={{ color: "red" }}>
+          {errors.username ? errors.username : null}
+        </span>
+        <br />
+        <label htmlFor="password">Password:</label>
         <input
           id="password"
           type="password"
@@ -48,7 +68,15 @@ export default function FormLogin() {
           onChange={handleChange}
         />
         <br />
-        <input type="submit" value="Enviar" />
+        <span style={{ color: "red" }}>
+          {errors.password && errors.password}
+        </span>
+        <br />
+        <input
+          type="submit"
+          value="Enviar"
+          disabled={errors.username || errors.password}
+        />
       </form>
     </div>
   );
---
+++ M3/L10/ArielRomero/Demo1/src/helpers/validateUser.js
@@ -0,0 +1,44 @@
+export const validateUser = ({ username, password }) => {
+  const regexEmail = /\S+@\S+\.\S+/; // texto@texto.texto
+  const regexLetras = /(?=.*[A-Za-z])/; // Al menos una letra
+  const regexNumeros = /(?=.*\d)/; // Al menos un número
+  const regexEspeciales = /(?=.*[@$!%*?&#])/; // @$!%*?&# al menos uno de estos caracteres
+
+  const errors = {};
+
+  // Validar username
+  if (!username) {
+    errors.username = "Username es requerido";
+  } else {
+    if (!regexEmail.test(username)) {
+      errors.username = "Username debe ser un email";
+    }
+  }
+
+  // Validar password
+  if (!password) {
+    errors.password = "Password es requerido";
+  } else {
+    if (password.length < 4 || password.length > 6) {
+      errors.password = "Password debe tener entre 4 y 6 caracteres";
+    }
+    if (!regexLetras.test(password)) {
+      errors.password = "Password debe tener al menos una letra";
+    }
+    if (!regexNumeros.test(password)) {
+      errors.password = "Password debe tener al menos un número";
+    }
+    if (!regexEspeciales.test(password)) {
+      errors.password =
+        "Password debe tener al menos un caracter especial (@$!%*?&#)";
+    }
+  }
+
+  return errors;
+};
+console.log(
+  validateUser({
+    username: "e@c.c",
+    password: "1%ae",
+  })
+);
