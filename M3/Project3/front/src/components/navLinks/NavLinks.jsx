f1b7570b8fbb63f4c5bd6f6f789fe16fd7c9df1b
sangaray <scgaraycochea@yahoo.com.ar>
2025-04-06 02:19:38 -0300

    Project3 - se agregó una opción de logout en la navbar

--- M3/Project3/front/src/components/navLinks/NavLinks.jsx
+++ M3/Project3/front/src/components/navLinks/NavLinks.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./NavLinks.module.css";
 import subjectData from "../../helpers/subjectData";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import AppointmentForm from "../../views/appointmentForm/AppointmentForm";
 
 const NavLinks = () => {
   const login = useSelector((state) => state.actualUser.userData.login);
   console.log(login);
 
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    const confirmed = window.confirm(
+      "¿Estás seguro que quieres cerrar sesión?"
+    );
+    if (confirmed) {
+      dispatch({ setUserData: { login: false, user: {} } });
+    }
+  };
   return (
     <div className={styles.container}>
       <Link to="/home">
@@ -45,6 +55,16 @@ const NavLinks = () => {
       <Link to="/contact">
         <span>Contacto</span>
       </Link>
+
+      {login ? (
+        <Link to="/">
+          <span onClick={handleLogout}>Salir</span>
+        </Link>
+      ) : (
+        <Link to="/">
+          <span>Ingresar</span>
+        </Link>
+      )}
     </div>
   );
 };
