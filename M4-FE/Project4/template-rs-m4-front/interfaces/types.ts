258cf44e0520fc557939bd6e0a4610d99d472b97
sangaray <scgaraycochea@yahoo.com.ar>
2025-06-05 16:24:29 -0300

    Project4 - Se creó las interfaces de product y de category

---
+++ M4-FE/Project4/template-rs-m4-front/interfaces/types.ts
@@ -0,0 +1,14 @@
+export interface IProduct {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  image: string[];
+  categoryId: number;
+}
+
+export interface ICategory {
+  id: number;
+  name: string;
+}
