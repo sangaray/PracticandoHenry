96f6e603ba5b18c7e9032b75ed06ff83da15ec81
Sandra Garaycochea <scgaraycochea@yahoo.com.ar>
2025-07-10 17:08:41 -0300

    M4FE-L3 - Daiana: trabajamos rutas en nextjs

--- M4-FE/L3/Daiana Grillia/next-project/src/app/demo/demo.tsx
+++ M4-FE/L3/Daiana Grillia/next-project/src/app/demo/demo.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 
 // components
-import { Card } from "../../components/card";
-import Button from "../../components/button";
+import Card from "../../components/card";
 
-const demo: React.FC = () => {
+const Demo: React.FC = () => {
   return (
     <div>
       <h1>Hola Mundo</h1>
-      <Card title="Card 1" description="This is card 1" />
-      <Card title="Card 2" description="This is card 2" />
+      <Card title="Card 1" description="Desacription for Card 1" />
+      <Card title="Card 2" description="Desacription for Card 2" />
       <Card title="Card 3" />
-      <Button />
     </div>
   );
 };
 
-export default demo;
+export default Demo;
