693b988596fb42c8e581dabfc2e420c06727b9bc
Sandra Garaycochea <scgaraycochea@yahoo.com.ar>
2025-07-17 16:56:19 -0300

    Porject4 - Se ajustaron algunas configuraciones y se creo la vista Home de la aplicación

---
+++ M4-FE/Project4/template-rs-m4-front/front/src/helpers/products.ts
@@ -0,0 +1,62 @@
+const productsToPreLoad: IProduct[] = [
+  {
+    name: "iPhone 11",
+    price: 699,
+    description:
+      "Experience power and elegance with the iPhone 11: capture stunning moments with its dual-camera system, enjoy exceptional performance, and immerse yourself in a brilliant Liquid Retina display. Discover a world of possibilities in the palm of your hand!",
+    image:
+      "https://res.cloudinary.com/dqdrtbtuv/image/upload/v1749073485/HenryProjects/Project4/IPhone11-removebg-preview_zvz2e5.png",
+    categoryId: 1,
+    stock: 10,
+  },
+  {
+    name: "MacBook Air",
+    price: 999,
+    description:
+      "Embrace efficiency and sophistication with the MacBook Air: lightweight design meets powerful performance, stunning Retina display brings your work to life, and all-day battery life keeps you productive wherever you go. Elevate your computing experience with the MacBook Air.",
+    image:
+      "https://res.cloudinary.com/dqdrtbtuv/image/upload/v1749073485/HenryProjects/Project4/MacBook_Air-removebg-preview_v13els.png",
+    categoryId: 2,
+    stock: 10,
+  },
+  {
+    name: "iPad Pro",
+    price: 799,
+    description:
+      "Unleash your creativity and productivity with the iPad Pro: powerful performance, stunning Liquid Retina display, and all-day battery life make the iPad Pro the perfect tool for work and play. Transform your ideas into reality with the iPad Pro.",
+    image:
+      "https://res.cloudinary.com/dqdrtbtuv/image/upload/v1749073485/HenryProjects/Project4/iPad_Pro-removebg-preview_cckzhn.png",
+    categoryId: 3,
+    stock: 10,
+  },
+  {
+    name: "Apple Watch Series 6",
+    price: 399,
+    description:
+      "Stay connected and healthy with the Apple Watch Series 6: track your workouts, monitor your health, and stay in touch with the people and information you care about most. Experience the future of health and wellness with the Apple Watch Series 6.",
+    image:
+      "https://res.cloudinary.com/dqdrtbtuv/image/upload/v1749073485/HenryProjects/Project4/Apple_Watch_Series_6-removebg-preview_c3ityu.png",
+    categoryId: 4,
+    stock: 10,
+  },
+  {
+    name: "AirPods Pro",
+    price: 249,
+    description:
+      "Immerse yourself in sound with the AirPods Pro: active noise cancellation, transparency mode, and customizable fit make the AirPods Pro the perfect companion for music, calls, and everything in between. Elevate your audio experience with the AirPods Pro.",
+    image:
+      "https://res.cloudinary.com/dqdrtbtuv/image/upload/v1749073485/HenryProjects/Project4/AirPods_Pro-removebg-preview_l8hzqs.png",
+    categoryId: 5,
+    stock: 10,
+  },
+  {
+    name: "HomePod mini",
+    price: 99,
+    description:
+      "Elevate your home audio experience with the HomePod mini: immersive sound, intelligent assistant, and smart home hub make the HomePod mini the perfect addition to your home. Enjoy a world of music, news, and more with the HomePod mini.",
+    image:
+      "https://res.cloudinary.com/dqdrtbtuv/image/upload/v1749073485/HenryProjects/Project4/HomePod_mini-removebg-preview_oazajy.png",
+    categoryId: 6,
+    stock: 10,
+  },
+];
