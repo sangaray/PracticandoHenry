4ed130aadb41e89aa98257b093ca91a540f156bb
Sandra Garaycochea <scgaraycochea@yahoo.com.ar>
2025-07-21 14:17:23 -0300

    M4FE-L4: Daiana - routing-next: se creó una ruta estática

---
+++ M4-FE/L4/Daiana Grillia/routing-next/src/app/estatica/Estatica.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Estatica = () => {
+  return (
+    <div>Estamos en nuestra ruta Estatica: https://localhost:3000/estatica</div>
+  );
+};
+
+export default Estatica;
