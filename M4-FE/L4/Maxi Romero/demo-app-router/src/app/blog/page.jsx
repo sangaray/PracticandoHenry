140fad2c7d82e9425a9acf347314bdc5a57402b7
Sandra Garaycochea <scgaraycochea@yahoo.com.ar>
2025-07-22 10:58:36 -0300

    M4FE-L4 - Maxi demo-app-router: Se creó una ruta estática

---
+++ M4-FE/L4/Maxi Romero/demo-app-router/src/app/blog/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Blog = () => {
+  return <div>Soy el Blog</div>;
+};
+
+export default Blog;
---
+++ M4-FE/L4/Maxi Romero/demo-app-router/src/app/estatica/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Estatica = () => {
+  return (
+    <div>Soy la ruta estática, estamos en http://localhost:3000/estatica</div>
+  );
+};
+
+export default Estatica;
