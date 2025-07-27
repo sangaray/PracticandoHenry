d450584260af0180ecb152b0a359961a6bfac9f7
Sandra Garaycochea <scgaraycochea@yahoo.com.ar>
2025-07-27 20:10:46 -0300

    M4FE-L5 - Daiana next-tailwind: se trabajó con tailwind en el archivo pages

--- M4-FE/L5/Daiana Grillia/next-tailwind/src/app/page.js
+++ M4-FE/L5/Daiana Grillia/next-tailwind/src/app/page.js
@@ -1,16 +1,24 @@
-import Image from "next/image";
-import CardWithCSS from "./CardWithCSS/CardWithCSS";
-import CardWithTailwind from "./CardWithTailwind/CardWithTailwind";
-
 export default function Home() {
   return (
-    <div style={{ display: "flex", flexDirection: "row" }}>
+    <div>
+      <p class="text-lg bg-blue-500 text-blue-200">Ese es un gran texto azul</p>
+      <div class="m-4 p-2">Contenido con margen y relleno</div>
+
+      <button class="bg-blue-400 hover:bg-blue-700">Botón</button>
+
+      <div value="w-full md:w-1/2">Este div ocupa x</div>
+
+      {/*<div class="div-con-margen-y-relleno">Contenido con margen y relleno</div>
+      <p className="parrafo-grande-azul">Ese es un gran texto azul</p>
+      <div style={{ display: "flex", flexDirection: "" }}>
       <div>
         <CardWithCSS />
       </div>
       <div>
         <CardWithTailwind />
-      </div>
+      </div> 
+      4 = 1rem = 16px
+      2 = 0.5rem = 8px*/}
     </div>
   );
 }
