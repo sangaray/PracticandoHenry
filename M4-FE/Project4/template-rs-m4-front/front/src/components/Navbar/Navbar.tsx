a6846668b90f2dd421e4f32de94eaa87448bd070
Sandra Garaycochea <scgaraycochea@yahoo.com.ar>
2025-08-01 20:14:24 -0300

    Project4: Se dio estilo al navbar utilizando estilos de la comunidad

--- M4-FE/Project4/template-rs-m4-front/front/src/components/Navbar/Navbar.tsx
+++ M4-FE/Project4/template-rs-m4-front/front/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,59 @@
 import React from "react";
 
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="mx-auto w-full max-w-screen-md border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3x1 lg:max-w-screen-lg">
+      <div className="px-4">
+        <div className="flex items-center justify-between">
+          <div className="flex shrink-0">
+            <a aria-current="page" className="flex items-center" href="/">
+              <img
+                className="h-12 w-auto rounded-full"
+                src="https://res.cloudinary.com/dqdrtbtuv/image/upload/v1754083565/HenryProjects/Project4/Dreamed_Products_t1mlrn.png"
+                alt="Logo de Dreamed Products"
+              />
+              <p className="sr-only">Website Title</p>
+            </a>
+          </div>
+          <div className="hidden md:flex md:items-center md:justify-center md:gap-5">
+            <a
+              aria-current="page"
+              className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+              href="#">
+              IPhones
+            </a>
+            <a
+              className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+              href="#">
+              Laptops
+            </a>
+            <a
+              className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+              href="#">
+              Tablets
+            </a>
+            <a
+              className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+              href="#">
+              Accesories
+            </a>
+          </div>
+          <div className="flex items-center justify-end gap-3">
+            <a
+              className="hidden items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
+              href="/login">
+              Sign in
+            </a>
+            <a
+              className="inline-flex items-center justify-center rounded-xl bg-[#E9AD64] px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-[#F3E4C7] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 hover:text-black"
+              href="/login">
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
