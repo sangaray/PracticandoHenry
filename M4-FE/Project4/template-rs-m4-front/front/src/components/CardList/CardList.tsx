9265365b36e79babb6676cfeaa02cbe0133b3db1
Sandra Garaycochea <scgaraycochea@yahoo.com.ar>
2025-08-21 14:26:18 -0300

    Project4: Se linkeó cada producto con su detalle

--- M4-FE/Project4/template-rs-m4-front/front/src/components/CardList/CardList.tsx
+++ M4-FE/Project4/template-rs-m4-front/front/src/components/CardList/CardList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Card from "@/components/Card/Card";
 import { getProductsDB } from "@/helpers/productsHelper";
+import Link from "next/link";
 
 const CardList = async () => {
   const products = await getProductsDB();
@@ -9,7 +10,11 @@ const CardList = async () => {
     <div className="flex flex-row flex-wrap justify-center gap-2">
       {products &&
         products?.map((product) => {
-          return <Card key={product.id} {...product} />;
+          return (
+            <Link key={product.id} href={`/products/${product.id}`}>
+              <Card {...product} />;
+            </Link>
+          );
         })}
     </div>
   );
