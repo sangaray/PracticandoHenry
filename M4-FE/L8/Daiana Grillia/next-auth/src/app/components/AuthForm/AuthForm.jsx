cd3c645fc6ed2fe320365276c055549a4ab00f53
Sandra Garaycochea <scgaraycochea@yahoo.com.ar>
2025-08-26 17:05:30 -0300

    M4FE-L8 - Daiana - next-auth: se trabajó el auth usando local storage

--- M4-FE/L8/Daiana Grillia/next-auth/src/app/components/AuthForm/AuthForm.jsx
+++ M4-FE/L8/Daiana Grillia/next-auth/src/app/components/AuthForm/AuthForm.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+"use client";
 
-const AuthForm = () => {
-  return <div>AuthForm</div>;
+import { useState } from "react";
+import styles from "./AuthForm.module.css";
+const AuthForm = ({ token, setToken }) => {
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const inputHandler = (event) => {
+    const { name, value } = event.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // alert(`Username: ${userData.username}, Password: ${userData.password}`);
+
+    fetch("https://fakestoreapi.com/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setToken(json.token);
+        localStorage.setItem("userToken", json.token);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={submitHandler}>
+        <div className={styles.inputContainer}>
+          <label htmlFor="username">Username: </label>
+          <input
+            name="username"
+            type="text"
+            required
+            onChange={inputHandler}
+            value={userData.username}
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="password">Password: </label>
+          <input
+            name="password"
+            type="password"
+            required
+            onChange={inputHandler}
+            value={userData.password}
+          />
+        </div>
+        <div className={styles.buttonContainer}>
+          <button className={styles.boton}>Login</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AuthForm;
--- M4-FE/L8/Daiana Grillia/next-auth/src/app/components/Navbar/Navbar.jsx
+++ M4-FE/L8/Daiana Grillia/next-auth/src/app/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+"use client";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.headerContainer}>
+        <h1 className={styles.title}>My Shopping App</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
